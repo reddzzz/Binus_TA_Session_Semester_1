d45708705eddaacb70ce98411783f2db748815fc
Sreeya Reddy <sreeyareddy@Sreeyas-MacBook-Pro.local>
2018-09-18 14:20:01 +0700

    First Commit

---
+++ Class2.py
@@ -0,0 +1,20 @@
+inp = int(input("insert a number:"))
+
+for i in range (1, inp+1):
+   print("*" * i)
+
+inp = int(input("insert a number:"))
+for i in range (1, inp +1):
+    print("*" * (inp + 1 - i))
+
+inp = int(input("insert a number:"))
+for i in range (1, inp+1):
+    print(""* (inp-i),("*" * i))
+
+inp = int(input("insert a number:"))
+for i in range (1, inp+1):
+    print("*" * (inp + 1 - i))
+
+
+
+
